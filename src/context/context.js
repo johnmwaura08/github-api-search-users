907a2d08f2fb346a72e4fda0b2bd7b2f2a109996
John Mwaura <johnmwaura08@gmail.com>
2020-10-06 13:51:58 -0500

    set up context to test out if value will be passed down to chikdren

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,19 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = React.createContext();
+
+// i have access to a provider and a consumer, i wont use a consumer now because of usecontext hook
+
+const GithubProvider= ({children}) => {
+
+    return (<GithubContext.Provider value={'hello'}>
+        {children}
+    </GithubContext.Provider>
+    );
+};
+
+export {GithubProvider, GithubContext};
+//my github provider is returning the provider from github context and i'll wrap the whole application in it at index.js
+//i'm also exporting github context so i can make use of usecontext  which now replaces the consumer
