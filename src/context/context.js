70e4bc254974a80fce3f55f1e5b0e16420218cf8
John Mwaura <johnmwaura08@gmail.com>
2020-10-07 13:15:24 -0500

    added state to context to use static data in the meantime

--- src/context/context.js
+++ src/context/context.js
@@ -10,14 +10,19 @@ const GithubContext = React.createContext();
 
 // i have access to a provider and a consumer, i wont use a consumer now because of usecontext hook
 
-const GithubProvider= ({children}) => {
+// i'm using the static data first before i do the ajax requests
 
-    return (<GithubContext.Provider value={'hello'}>
+const GithubProvider = ({ children }) => {
+    const [githubUser, setGithubUser] = useState(mockUser);
+    const [repos, setRepos] = useState(mockRepos);
+    const [followers, setFollowers] = useState(mockFollowers);
+
+    return (<GithubContext.Provider value={{githubUser, repos, followers}}>
         {children}
     </GithubContext.Provider>
     );
 };
 
-export {GithubProvider, GithubContext};
+export { GithubProvider, GithubContext };
 //my github provider is returning the provider from github context and i'll wrap the whole application in it at index.js
 //i'm also exporting github context so i can make use of usecontext  which now replaces the consumer
