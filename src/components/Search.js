0b3ef1efed295b555d01e37ee695d534fefbf706
John Mwaura <johnmwaura08@gmail.com>
2020-10-08 23:22:37 -0500

    imported error from context js added a new if statement to show the search button only when the rate limit is greater than zero

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,24 +4,32 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('');
+  const { requests, error} = React.useContext(GithubContext);
   //get things from global context
   const handleSubmit =(e) => {
     e.preventDefault();
     if(user){
-      
+
     }
     console.log(user);
   }
   return <section className="section">
     <Wrapper className="section-center">
+      {error.show && 
+      <ErrorWrapper>
+        <p>{error.msg}</p>
+      </ErrorWrapper>
+        }
       <form onSubmit={handleSubmit}>
         <div className="form-control">
           <MdSearch />
           <input type="text" placeholder="enter github user" value={user} onChange={(e) => setUser(e.target.value)}/>
+          {requests > 0 &&   
           <button type="submit">Search</button>
+          }
         </div>
       </form>
-      <h3>requests: 60 /60 </h3>
+      <h3>requests: {requests} /60 </h3>
     </Wrapper>
   </section>
 };
