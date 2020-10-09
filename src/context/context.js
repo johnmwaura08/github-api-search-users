3fa7ae2d416d63715f9ea06b852fbb3ffb2952ed
John Mwaura <johnmwaura08@gmail.com>
2020-10-08 22:28:31 -0500

    added axios request to get rate limit

--- src/context/context.js
+++ src/context/context.js
@@ -16,6 +16,24 @@ const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
+    //request loading
+    const [ requests, setRequests] = useState(0);
+    const [loading, setIsLoading] = useState(false)
+    //check rate
+    const checkRequests = () => {
+
+        axios.get(`${rootUrl}/rate_limit`)
+            .then(({data}) => {
+                let {rate: {remaining}} = data;
+                setRequests(remaining)
+                if(remaining === 0 ){
+                    //throw an error
+                }
+            })
+            .catch((err) => console.log(err))
+    }
+    //error
+    useEffect(checkRequests, [])
 
     return (<GithubContext.Provider value={{githubUser, repos, followers}}>
         {children}
