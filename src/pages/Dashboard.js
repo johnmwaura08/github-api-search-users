94fa2962bc09453b6fd75ac73a8c6e77f9581f73
John Mwaura <johnmwaura08@gmail.com>
2020-10-09 23:21:53 -0500

    cleanup and linter work

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
-import Events from '../components/Events';
+
 
 const Dashboard = () => {
   const {isLoading} = React.useContext(GithubContext)
