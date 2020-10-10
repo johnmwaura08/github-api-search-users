6ea15ab43bdd7c7ff93f9c9877dfcb449aebe6ed
John Mwaura <johnmwaura08@gmail.com>
2020-10-09 19:45:23 -0500

    added promise.allsettled so as to show info once all the axios calls are done

--- src/context/context.js
+++ src/context/context.js
@@ -17,23 +17,41 @@ const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
-    const [ events, setEvents] = useState(mockEvents)
+    const [events, setEvents] = useState(mockEvents)
     //request loading
     const [requests, setRequests] = useState(0);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     //error
-    const [error, setError] = useState({show: false, msg:""})
+    const [error, setError] = useState({ show: false, msg: "" })
 
     const searchGithubUser = async (user) => {
-        //toggle error
         toggleError();
         setIsLoading(true);
-        //setloading true
+      
         const response = await axios.get(`${rootUrl}/users/${user}`)
-                                    .catch(err => console.log(err))
-        if(response){
-            setGithubUser(response.data)
-            //more logic here
+            .catch(err => console.log(err));
+
+        if (response) {
+            setGithubUser(response.data);
+            const { login, followers_url } = response.data;
+
+            await Promise.allSettled([
+                axios.get(`${rootUrl}/users/${login}/repos?per_page=100`),
+                axios.get(`${followers_url}?per_page=100`),
+                axios.get(`${rootUrl}/users/${login}/events`)
+            ]).then((results) => {
+                const [repos, followers, events] = results;
+                const status = 'fulfilled';
+                if (repos.status === status) {
+                    setRepos(repos.value.data)
+                }
+                if (followers.status === status) {
+                    setFollowers(followers.value.data)
+                }
+                 if (events.status === status) {
+                    setEvents(events.value.data)
+                }
+            }).catch(err => console.log(err))
         }
         else {
             toggleError(true, 'there is no user with that username')
@@ -56,13 +74,13 @@ const GithubProvider = ({ children }) => {
             .catch((err) => console.log(err))
     }
 
-    function toggleError(show = false, msg=""){
-        setError({show, msg})
+    function toggleError(show = false, msg = "") {
+        setError({ show, msg })
     }
 
     useEffect(checkRequests, [])
 
-    return (<GithubContext.Provider value={{ githubUser, repos, followers, requests, error,events,searchGithubUser, isLoading }}>
+    return (<GithubContext.Provider value={{ githubUser, repos, followers, requests, error, events, searchGithubUser, isLoading }}>
         {children}
     </GithubContext.Provider>
     );
