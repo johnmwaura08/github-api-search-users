5585a07c0d6352b45355d90fded190dc0f47f7ff
John Mwaura <johnmwaura08@gmail.com>
2020-10-09 19:46:37 -0500

    added routes to dashboard and error components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './App.css';
 import Dashboard from '../src/pages/Dashboard';
 import Login from '../src/pages/Login';
-import Error from '../src/pages/Error'
-
+import Error from '../src/pages/Error';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-      <Login />
-      <Error />
+    <div>
+      <Router>
+        <Switch>
+          <Route path='/' exact={true}>
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path='/login'>
+            <Login></Login>
+          </Route>
+          <Route path='*'>
+            <Error></Error>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
