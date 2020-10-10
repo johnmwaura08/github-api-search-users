ad924c88572af2876b7e7390c2ac690c1bd10bbf
John Mwaura <johnmwaura08@gmail.com>
2020-10-09 22:52:06 -0500

    wrapped the entire application using authwrapper

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import Dashboard from '../src/pages/Dashboard';
 import Login from '../src/pages/Login';
 import Error from '../src/pages/Error';
 import PrivateRoute from '../src/pages/PrivateRoute';
+import AuthWrapper from '../src/pages/AuthWrapper'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <PrivateRoute path='/' exact={true}>
-            <Dashboard></Dashboard>
-          </PrivateRoute>
-          <Route path='/login'>
-            <Login></Login>
-          </Route>
-          <Route path='*'>
-            <Error></Error>
-          </Route>
-        </Switch>
-      </Router>
+      <AuthWrapper>
+        <Router>
+          <Switch>
+            <PrivateRoute path='/' exact={true}>
+              <Dashboard></Dashboard>
+            </PrivateRoute>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route path='*'>
+              <Error></Error>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthWrapper>
+
     </div>
   );
 }
