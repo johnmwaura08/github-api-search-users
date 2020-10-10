8568f3920934085e1d3f524723e5af4ca5a6dcf7
John Mwaura <johnmwaura08@gmail.com>
2020-10-09 21:36:44 -0500

    restricted dashboard to viewing only when user is logged in

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = () => {
-  return <h2>private route component</h2>;
+const PrivateRoute = ({children,...restOfTheProps}) => {  //the children will be the dashboard
+  const { isAuthenticated, user } = useAuth0();
+  const isUser = isAuthenticated && user;
+
+  return (
+    <Route 
+    {...restOfTheProps} 
+    render={() => {
+      return isUser? children: <Redirect to='/login'/>
+    }}
+    >
+    </Route>
+  );
+
 };
 export default PrivateRoute;
